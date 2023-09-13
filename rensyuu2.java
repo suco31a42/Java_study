0646d9d997b015fdff4f3cf1272745ed80e52ed6
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-13 12:34:04 +0000

    rensyuu2

---
+++ rensyuu2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Exercise {
+  public static void main( String[] args ) throws IOException {
+    BufferedReader br = new BufferedReader(
+      new InputStreamReader( System.in ) );
+      String s = br.readLine();
+      
+  }
+}
